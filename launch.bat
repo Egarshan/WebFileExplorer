4f9dd7a615957c43fe69bbd1175ce4d90e7883c8
Egor <varich.net@gmail.com>
2019-11-02 03:50:20 +0700

    Final (i hope) commit
    *fix launch file
    *added solves of possible errors
    *update of README

--- launch.bat
+++
@@ -1,2 +0,0 @@
-pip install -r requirements.txt
-powershell -noexit "./to_do"
---
+++ launcher.bat
@@ -0,0 +1,6 @@
+pip install -r requirements.txt
+cd source
+start "" http://127.0.0.1:8000/start_page/
+python manage.py runserver
+
+
